4d7d9f34857c76be1e51ad4456f6f12e38769f03
Robert Kaye <rob@musicbrainz.org>
2014-10-12 00:27:54 +0200

    Stumped on how to fix this problem. :-(

--- test_control.py
+++ test_control.py
@@ -6,14 +6,23 @@ from time import sleep
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("10.10.15.158", 8888))
 
-steps = list(range(-20, 22, 2))
-bsteps = list(range(-20, 22, 2))
+steps = list(range(-10, 22, 2))
+bsteps = list(range(-10, 22, 2))
 bsteps.reverse()
 
-while True:
+s.send("volume .5;")
+s.send("speed0 1;")
+s.send("speed1 1;")
+
+while 0:
     for i in steps:
        s.send("speed0 %.8f;" % (i / 10.0))
-       sleep(.5)
+       sleep(1)
     for i in bsteps:
        s.send("speed0 %.8f;" % (i / 10.0))
-       sleep(.5)
+       sleep(1)
+
+while True:
+    for i in range(0, 11):
+       s.send("crossfade %.8f;" % (i / 10.0))
+       sleep(1)
