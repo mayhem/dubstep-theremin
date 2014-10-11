7f231e2207bcd16fd08d44cc07cbc65a141d63e6
Cedric Mao <cedric.mao@gmail.com>
2014-10-11 13:56:15 +0200

    Volume messages ON in lm-reader
    lm-reader updated with
     - Volume control
     - Instrument selection with SREEN_TAP
     - Messages sent for volume control

--- lm-reader.py
+++ lm-reader.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
     
-import os, sys, inspect, thread, time
+import os, sys, inspect, thread, time, math, OSC
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = 'lib'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+from math import floor
 
 class SampleListener(Leap.Listener):
     def __init__(self):
@@ -15,6 +16,25 @@ class SampleListener(Leap.Listener):
         self.lp_normal = None
         self.rp_pos = None
         self.lp_pos = None
+        self.volume = 0
+
+        #Defining the instrument selection grid
+        self.CONST_GRID_COL_NUMBER = 2
+        self.CONST_GRID_LINE_NUMBER = 2
+        self.instrumentnumber = self.CONST_GRID_COL_NUMBER * self.CONST_GRID_LINE_NUMBER
+        self.st_position = None
+        self.instrument_selection_x = 0
+        self.instrument_selection_y = 0
+        self.instrument_x = 0
+        self.instrument_y = 0
+        self.instrument = 0
+
+        #Setting OSC
+        self.dest = "10.10.15.159", 7777
+        self.c = OSC.OSCClient()
+        self.c.connect(self.dest)
+        
+        #self.instrumentlist = List[i for i in range(self.instrumentnumber)]
 
     def on_connect(self, controller):
         print "Connected"
@@ -25,10 +45,24 @@ class SampleListener(Leap.Listener):
         for gesture in gestures:
             if gesture.is_valid and gesture.type == Leap.Gesture.TYPE_SWIPE and gesture.state == Leap.Gesture.STATE_STOP:
                 self.enabled = not self.enabled
+                print "Welcome !"
+                print "Please select your instrument by pointing one on the grid."
                 return
+            #Instrument selection    
+            if gesture.is_valid and gesture.type == Leap.Gesture.TYPE_SCREEN_TAP and gesture.state == Leap.Gesture.STATE_STOP:
+                self.st_position = Leap.ScreenTapGesture(gesture).position
+                self.instrument_selection_x = min(100, max(0,(self.st_position.x + 150)/3.5 ))
+                self.instrument_selection_y = min(100, max(0,(self.st_position.y - 40)/3.6 ))
+                self.instrument_x = floor(self.instrument_selection_x / (100 / self.CONST_GRID_COL_NUMBER)) + 1
+                self.instrument_y = floor(self.instrument_selection_y / (100 / self.CONST_GRID_LINE_NUMBER)) + 1
+                self.instrument = self.instrument_x * self.instrument_y
+                print "instrmuent selected :" + str(self.instrument)
+
+
 
         if not self.enabled:
             return
+        
 
         handlist = frame.hands
         for hand in handlist:
@@ -39,8 +73,13 @@ class SampleListener(Leap.Listener):
                 if hand.is_left:
                     self.lp_normal = hand.palm_normal
                     self.hp_pos = hand.palm_position
+        
+        volume = min(100, max(0, (self.rp_pos.y - 40)/3.6))
+        self.msg = OSC.OSCMessage()
+        self.msg.setAddress("/volume")
+        self.msg.append(volume)
+        self.c.send(self.msg)
 
-        print self.rp_pos
 
 #        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
 #            frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
@@ -48,10 +87,12 @@ class SampleListener(Leap.Listener):
 listener = SampleListener()
 controller = Leap.Controller()
 controller.enable_gesture(Leap.Gesture.TYPE_SWIPE)
+controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP)
 controller.add_listener(listener)
 
 controller.config.set("Gesture.Swipe.MinLength", 100.0)  # in mm
 controller.config.set("Gesture.Swipe.MinVelocity", 750)  # in mm/s
+controller.config.set("Gesture.ScreenTap.MinForwardVelocity", 20.0) # in mm/s
 controller.config.save()
 
 try:
